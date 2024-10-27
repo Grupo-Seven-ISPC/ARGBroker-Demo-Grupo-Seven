bae2dcf5259ee8396075351ff623ecebe25d2023
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2024-10-27 16:28:19 -0300

    Creacion archivo de consultas SELECT

---
+++ Proyecto Integrador ARGBroker Demo/Base de Datos/Consultas_SELECT.sql
@@ -0,0 +1,18 @@
+--Consultas tipo SELECT
+--1.Seleccionar simbolo y cantidad de acciones
+SELECT simbolo, cantidad FROM Accion;
+
+--2.Mostrar apellido y nombre de los usuarios
+SELECT apellido, nombre FROM Usuarios;
+
+--3.Mostrar símbolo de las 3 acciones con menor cantidad en stock
+SELECT simbolo FROM Accion 
+ORDER BY cantidad ASC LIMIT 3;
+
+--4.Mostrar primeras 10 operaciones realizadas
+SELECT * FROM Operacion
+ORDER BY fecha ASC LIMIT 10;
+
+--5.Mostrar las operaciones con volúmenes operados menores a 10 acciones
+SELECT * FROM Operacion
+WHERE cantidad < 10;
