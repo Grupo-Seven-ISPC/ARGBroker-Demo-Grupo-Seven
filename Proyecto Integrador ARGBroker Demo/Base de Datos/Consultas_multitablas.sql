ea557661e5dc143fe88d0418e6c3b04f330809f7
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2024-10-27 20:38:12 -0300

    Update Consultas_multitablas.sql
    Corrección menor (sintaxis de comentarios)

--- Proyecto Integrador ARGBroker Demo/Base de Datos/Consultas_multitablas.sql
+++ Proyecto Integrador ARGBroker Demo/Base de Datos/Consultas_multitablas.sql
@@ -1,5 +1,5 @@
---Consultas multitablas
---1.Se solicita mostrar los usuarios (id, nombre y apellido) que realizaron operaciones con Mirgor
+-- Consultas multitablas
+-- 1.Se solicita mostrar los usuarios (id, nombre y apellido) que realizaron operaciones con Mirgor
 
 SELECT u.id_usuario, u.nombre, u.apellido 
 FROM Usuarios u 
@@ -10,7 +10,7 @@ JOIN (
     WHERE a.nombre = 'Mirgor'
 ) sc ON u.id_usuario = sc.id_usuario;
 
---2.Se solicita mostrar los usuarios (nombre y apellido) con operaciones pendientes
+-- 2.Se solicita mostrar los usuarios (nombre y apellido) con operaciones pendientes
 
 SELECT u.nombre, u.apellido 
 FROM Usuarios u 
@@ -21,7 +21,7 @@ JOIN (
     WHERE e.estado = 'pendiente'
 ) sc ON u.id_usuario = sc.id_usuario;
 
---3.Se solicita mostrar las 3 acciones (símbolo y total de operaciones) con más operaciones
+-- 3.Se solicita mostrar las 3 acciones (símbolo y total de operaciones) con más operaciones
 
 SELECT a.simbolo, COUNT(o.id_operacion) AS total_operaciones
 FROM Operacion o
